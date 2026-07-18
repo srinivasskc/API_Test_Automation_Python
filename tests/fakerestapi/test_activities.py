d5b6bd77ea121ffa630771f151f18c7010d87865
Srinivas Kadiyala <srinivas.skc30@gmail.com>
2026-07-18 11:30:56 +0530

    dynamic data json

--- tests/fakerestapi/test_activities.py
+++ tests/fakerestapi/test_activities.py
@@ -32,3 +32,34 @@ def test_post_activities_validation(api_client):
     print(response.json())
     assert response.status_code == 200
     assert response.json()["title"] == "Activity 29"
+
+
+def test_put_activities_validation(api_client):
+    update_activities_data = {
+        "id": 30,
+        "title": "Update Activity 30",
+        "dueDate": "2026-07-18T02:24:17.731Z",
+        "completed": True,
+    }
+
+    response = api_client.put_activities("api/v1/Activities/30", update_activities_data)
+    print(response.json())
+    assert response.status_code == 200
+    assert response.json()["title"] == "Update Activity 30"
+    assert len(response.json()) > 0
+
+    id = response.json()["id"]
+    response = api_client.get_activities("api/v1/Activities/30")
+    print(response.json())
+    assert response.status_code == 200
+    assert response.json()["title"] == "Activity 30"
+
+
+def test_delete_activities_validation(api_client):
+    response = api_client.get_activities("api/v1/Activities/30")
+    print(response.json())
+    assert response.status_code == 200
+    assert response.json()["title"] == "Activity 30"
+
+    response = api_client.delete_activities("api/v1/Activities/30")
+    assert response.status_code == 200
--- utils/api_client.py
+++ utils/api_client.py
@@ -2,10 +2,10 @@ import requests
 
 
 class APIClient:
-    BASE_URL = "https://fakerestapi.azurewebsites.net/"
+    BASE_URL = "https://fakerestapi.azurewebsites.net"
 
     def __init__(self):
-        self.header = {"accept": "text/plain; v=1.0"}
+        self.header = {"accept": "*/*; v=1.0"}
 
     def get_activities(self, endpoint):
         url = f"{self.BASE_URL}/{endpoint}"
@@ -13,6 +13,21 @@ class APIClient:
         return response
 
     def post_activities(self, endpoint, data):
+        url = f"{self.BASE_URL}/{endpoint}"
+        response = requests.post(url=url, headers=self.header, json=data, timeout=5000)
+        return response
+
+    def put_activities(self, endpoint, data):
+        url = f"{self.BASE_URL}/{endpoint}"
+        response = requests.put(url=url, headers=self.header, json=data, timeout=5000)
+        return response
+
+    def delete_activities(self, endpoint):
+        url = f"{self.BASE_URL}/{endpoint}"
+        response = requests.delete(url=url, headers=self.header, timeout=5000)
+        return response
+
+    def post_activities_dynamic(self, endpoint, data):
         url = f"{self.BASE_URL}/{endpoint}"
         response = requests.post(url=url, headers=self.header, json=data, timeout=5000)
         return response
